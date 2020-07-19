9ab8edd52c83946813e5b65bf525c5db4c1d821e
matheusysd <matheusyurisd@gmail.com>
2020-07-19 14:32:59 -0300

    Componentes da página inicial/login criados e renderizados

---
+++ src/componentes/Botao.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Botao = ({ texto, onClick, dataTestId, condition }) => {
+  return (
+    <div>
+      <button
+        data-testid={dataTestId}
+        disabled={condition}
+        onClick={onClick ? () => onClick() : null}
+        type="button"
+      >
+        {texto}
+      </button>
+    </div>
+  );
+};
+
+Botao.propTypes = {
+  texto: PropTypes.string.isRequired,
+  onClick: PropTypes.func,
+  dataTestId: PropTypes.string.isRequired,
+  condition: PropTypes.string,
+};
+
+Botao.defaultProps = {
+  onClick: null,
+  condition: false,
+};
+
+export default Botao;
--- src/paginas/PaginaInicial.js
+++ src/paginas/PaginaInicial.js
@@ -1,14 +1,35 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import Input from '../componentes/Inicial/Input';
+import Botao from '../componentes/Botao';
 
 class PaginaInicial extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      nome: '',
+      email: '',
+    };
+    this.changeHandler = this.changeHandler.bind(this);
+  }
+
+  changeHandler({ name, value }) {
+    this.setState({ [name]: value });
+    console.log(this.state);
   }
 
   render() {
+    const { nome, email } = this.state;
     return (
       <div data-testid="">
-        <span>Pagina Inicial</span>
+        <Input name="nome" onChange={this.changeHandler} dataTestId="input-player-name" />
+        <Input name="email" onChange={this.changeHandler} dataTestId="input-gravatar-email" />
+        <Link to="/game">
+          <Botao texto="Jogar" dataTestId="btn-play" condition={!nome || !email} />
+        </Link>
+        <Link to="/settings">
+          <Botao texto="Configuracoes" dataTestId="btn-settings" />
+        </Link>
       </div>
     );
   }
