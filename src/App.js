a82c4aa47ec43ee20c209d5042eabbf6642201e5
Hugo J. De Moro Jr <hugo.urano@gmail.com>
2020-07-20 16:34:30 -0300

    adicionado componente na rota do arquivo app

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import PaginaInicial from './paginas/PaginaInicial';
 import './App.css';
+import PaginaConfiguracoes from './paginas/PaginaConfiguracoes';
 
 export default function App() {
   return (
@@ -11,7 +12,7 @@ export default function App() {
         <Route exact path="/game" />
         <Route exact path="/feedback" />
         <Route exact path="/ranking" />
-        <Route exact path="/settings" />
+        <Route exact path="/settings" component={PaginaConfiguracoes} />
         <Route path="" />
       </Switch>
     </BrowserRouter>
