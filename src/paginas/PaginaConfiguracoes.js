20d849bd05ba032a87a82b3d6737c6e268008c70
Hugo J. De Moro Jr <hugo.urano@gmail.com>
2020-07-20 17:11:06 -0300

    adicionando pagina configuracoes

---
+++ src/paginas/PaginaConfiguracoes.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class PaginaConfiguracoes extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <h1 data-testid="settings-title">
+        Configurações:
+      </h1>
+    );
+  }
+}
+
+export default PaginaConfiguracoes;
