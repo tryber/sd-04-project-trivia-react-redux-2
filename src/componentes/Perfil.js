f2cb42bb6591d4ae8ca722df115039ad5ae424fb
Hugo J. De Moro Jr <hugo.urano@gmail.com>
2020-07-20 17:52:53 -0300

    criando componente perfil

---
+++ src/componentes/Perfil.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Perfil = ({ nome, imagem, pontos }) => (
+  <div>
+    <span>${nome}</span>
+    <img src="" />
+    <span>${pontos}</span>
+  </div>
+);
+
+Input.propTypes = {
+  nome: PropTypes.string.isRequired,
+  imagem: PropTypes.shape.isRequired,
+  pontos: PropTypes.number.isRequired,
+};
+
+export default Perfil;
