7e06f776814531f2bb73917c6202129211404870
Hugo J. De Moro Jr <hugo.urano@gmail.com>
2020-07-21 17:10:10 -0300

    refeita a branch do componente perfil

---
+++ src/componentes/Perfil.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Perfil = ({ nome, imagem, pontos }) => (
+  <div>
+    <span>${nome}</span>
+    <img src=${imagem} />
+    <span>${pontos}</span>
+  </div>
+);
+
+Input.propTypes = {
+  nome: PropTypes.string.isRequired,
+  imagem: PropTypes.string.isRequired,
+  pontos: PropTypes.number.isRequired,
+};
+
+export default Perfil;
