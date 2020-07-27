208c298b89c2f180f7ee68baf0d4085cba609301
Hugo J. De Moro Jr <hugo.urano@gmail.com>
2020-07-27 11:43:25 -0300

    esqueleto da pagina feedback

--- src/paginas/PaginaFeedback.js
+++ src/paginas/PaginaFeedback.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Perfil from '../componentes/Perfil';
+import Botao from '../componentes/Botao';
 
 const PaginaFeedback = () => (
   <div>
-    <Perfil />
+    <header>
+      <img src="" alt="" data-testid="header-profile-picture" />
+      <span data-testid="header-player-name"></span>
+      <span data-testid="header-score"></span>
+    </header>
+
+    <Link to="/">
+      <Botao texto="Jogar Novamente" dataTestId="btn-play-again" />
+    </Link>
+    <Link to="/">
+      <Botao texto="Ver Ranking" dataTestId="ranking-title" />
+    </Link>
   </div>
 );
 
